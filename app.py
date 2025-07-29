e6295fbcec96720f209eb89ebfb9eeef04e2930f
ana-calderon-r <anitabelcr@gmail.com>
2025-07-29 17:51:00 -0500

    app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.markdown("""
 st.markdown("""
     <style>
     div[data-baseweb="select"] > div {
-        background-color: #32CD32 !important;
+        background-color: #DFF5E1 !important;
         border-radius: 8px !important;
         border: 1px solid #74A84C !important;
     }
